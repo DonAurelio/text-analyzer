0a95e8788f6abf6a5c560743743ba341fa7d2a30
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2016-10-22 22:06:34 +0000

    Organizado

--- tkmorfo.py
+++ tkmorfo.py
@@ -28,13 +28,13 @@ op.set_data_files( "",
                    DATA + LANG + "/probabilitats.dat");
 
 # create analyzers
-tk=freeling.tokenizer(DATA+LANG+"/tokenizer.dat");
+f_tk=freeling.tokenizer(DATA+LANG+"/tokenizer.dat");
 #sp=freeling.splitter(DATA+LANG+"/splitter.dat");
 #sid=sp.open_session();
-mf=freeling.maco(op);
+f_mf=freeling.maco(op);
 
 # activate mmorpho odules to be used in next call
-mf.set_active_options(False, True, True, True,  # select which among created 
+f_mf.set_active_options(False, True, True, True,  # select which among created 
                       True, True, False, True,  # submodules are to be used. 
                       True, True, True, True ); # default: all created submodules are used
 
@@ -71,6 +71,10 @@ mf.set_active_options(False, True, True, True,  # select which among created
 # T con etiquetas Ti, el proceso de análisis morfológico tambien conocido como etiquetación,
 # consiste en asignar a cada palabra de la oración su etiqueta correspondiente, creando una 
 # lista de tuplas (s,t) donde s partenece a S y t pertenece a T
+
+#============================ Descripción del proyecto ==============================
+
+#============================ Definición Etiquetas EAGLES ===========================
 
 EAGLES_ADJETIVOS = {
   '1A':'Adjetivo',
@@ -192,30 +196,120 @@ EAGLES_PRONOMBRES = {
   '71':'1 Persona-sg',
   '72':'2 Persona-sg',
   '70':'3 Persona',
-  '74':'1 Persona-pl'
+  '74':'1 Persona-pl',
   '75':'2 Persona-pl',
   '8P':'Polite'
 }
 
+EAGLES_CONJUNCIONES = {
+  '1C':'Conjunción',
+  '2C':'Coordinada',
+  '2S':'Subordinada',
+  '30':'-',
+  '40':'-'
+}
 
-mensaje = "El músico bajo toca el bajo"
-mensaje1 = "Mi #Tbt hoy es con @MarcAnthony  y seguro les gustara quiero que continúen \
-ustedes con la letra. Cuando nos volvamos a encontrar :)"
-mensaje2 = "Por fin!! de nuevo en Twitter! =D se me daño mi celular pero ya tengo uno \
-nuevo =D @rosariomeneses1 creo que es igual al tuyo #emoticones XD <3 ^_^!"
+EAGLES_NUMERALES = {
+  '1M':'Numeral',
+  '2C':'Cardinal',
+  '2O':'Ordinal',
+  '3M':'Masculino',
+  '3F':'Femenino',
+  '3C':'Común',
+  '4S':'Singular',
+  '4P':'Plural',
+  '50':'-',
+  '6P':'Pronominal',
+  '6D':'Determinante',
+  '6A':'Adjetivo'
+}
 
-print(mensaje1)
-# INICIO DE PRUEBA
-def prueba_freeling():
+EAGLES_INTERJECCIONES = {
+  '1I':'Interjección'
+}
+
+EAGLES_ABREVIATURAS = {
+  '1Y':'Abreviratura'
+}
+
+EAGLES_PREPOSICIONES = {
+  '1S':'Adposición',
+  '2P':'Preposición',
+  '3S':'Simple',
+  '3C':'Contraída',
+  '3M':'Masculino',
+  '4S':'Singular'
+}
+
+EAGLES_SIGNOS_DE_PUNTUACION = {
+  '1F':'Puntuación'
+}
+
+#============================ Definición Etiquetas EAGLES ===========================
+
+#============================ Algoritmo Propuesto ===================================
+""" 
+    Iterar el arreglo que devuelve TweetTokenizer (lista de strings) y cada palabra
+    a word usando freeling.word() al finalizar crear un list<word>
+    
+    Parte dificil?:
+    Hacer a mano tagueo set_tag() de símbolos, nicknames, hashtags, urls y para
+    cada uno setear lock_analysis() para que sean ignorados por el analyze()
+
+    Convertir el list<word> a sentence, aplicar el análisis morfológico, 
+    crear estructura de datos para enviar a la aplicación web (interpretar etiquetas)
+    y mostrar
+
+ """
+
+ def to_freeling_lword(lis):
+  """
+    Lista de Strings a Lista de palabras
+    
+    Recibe una lista de strings y retorna una tupla de words list<word>.
+    ya que freeling trabaja con este tipo de colecciones.
+  """
+  lword = ()
+  for w in lis:
+    lword += (freeling.word(w),)
+
+  oracion = freeling.sentence(lword)
+  return oracion
+
+def procesar_texto_ntlk_freeling(texto):
+  # Segmentación de frases (sentencias) y palabras con nltk
+  # Tokenización
+  tknzr = TweetTokenizer()
+  tkn_mensaje = tknzr.tokenize(texto)
+
+def procesar_texto_nltk(texto):
+  print ("Usando tokenizacion de nltk: ")
+  # tokenizacion con TweetTokenizer
+  tknzr = TweetTokenizer()
+  tkn_mensaje = tknzr.tokenize(mensaje)
+  print (tkn_mensaje)
+  # crear list<word> y sentence
+  msg = to_freeling_lword(tkn_mensaje)
+  msg = freeling.sentence(msg)
+  # aplicar análisis morfo
+  msg = f_mf.analyze(msg)
+  #show_mf(msg)
+  for w in sentence:
+  print("Análisis de: ", w.get_form())
+  for w_a in w.get_analysis():
+    print (w_a.get_lemma())
+    print (w_a.get_tag())
+
+def procesar_texto_freeling(texto):
   # análisis morfológico solamente sobre las palabras del Español
   print ("Usando tokenizacion de freeling: ")
 
   # tokenize() de freeling retorna un list<word>
-  tk_msg = tk.tokenize(mensaje)
+  tk_msg = f_tk.tokenize(texto)
   # En este caso sentence recibe un list<word> y devuelve un sentence
   s_msg = freeling.sentence(tk_msg)
   # analyze recibe un sentence y me retorna un sentence analizado :P
-  s_msg = mf.analyze(s_msg)
+  s_msg = f_mf.analyze(s_msg)
 
   # retorna vector<word>
   ws = s_msg.get_words()
@@ -229,28 +323,6 @@ def prueba_freeling():
       print (a.get_tag())
 
 
-# ALGORITMO PROPUESTO:
-""" 
-    Iterar el arreglo que devuelve TweetTokenizer y "castear" cada palabra
-    en word usando freeling.word() al finalizar crear un list<word>
-    
-    Parte dificil?:
-    Hacer a mano tagueo set_tag() de símbolos, nicknames, hashtags, urls y para
-    cada uno setear lock_analysis() para que sean ignorados por el analyze()
-
-    Convertir el list<word> a sentence, aplicar el análisis morfológico, 
-    crear estructura de datos para enviar a la aplicación web (interpretar etiquetas)
-    y mostrar
-
- """
-
-# Recibe una lista de strings y retorna una tupla de words list<word>
-def to_lword(lis):
-  lword = ()
-  for w in lis:
-    lword += (freeling.word(w),)
-  return lword
-
 # Recibe un list<word> y taguea símbolos, nicknames, hashtags, urls
 def pre_mf_analyze():
   pass
@@ -261,61 +333,10 @@ def pre_mf_analyze():
   {"Categoría": "Adjetivo", Tipo": "Calificativo", "Grado": 0, "Género": "Común",
    "Número": "Plurar", "Caso":0, "Función":0}
  """
-def eagle2info(etiqueta):
-  # ADJETIVOS
-  if etiqueta[0] == "A":
-    if etiqueta[1] == "Q": 
-      tipo = "Calificativo"
-    if etiqueta[2] == "A":
-      grado = "Apreciativo"
-    elif etiqueta[2] == "0":
-      grado = 0
-    if etiqueta[3] == "M":
-      genero = "Masculino"
-    elif etiqueta[3] == "F":
-      genero = "Femenino"
-    elif etiqueta[3] == "C":
-      genero = "Común"
-    elif etiqueta[3] == "0":
-      genero = 0
-    if etiqueta[4] == "S":
-      num = "Singular"
-    elif etiqueta[4] == "P":
-      num = "Plural"
-    elif etiqueta[4] == "N":
-      num = "Invariable"
-    caso = 0
-    if etiqueta[6] == "P":
-      funcion = "Participio"
-    elif etiqueta[6] == "0":
-      funcion = 0
-    json_i = {"Categoría": "Adjetivo", "Tipo": tipo, "Grado": grado, \
-              "Género": genero, "Número": num, "Caso": caso, "Función": funcion}
-    return json_i
-  # ADVERBIOS
-  if etiqueta[0] == "R"
-    json_i = {"Categoría": "Adverbio", "Tipo": "General"}
-    return json_i
-  # ARTICULOS
 
-print (eagle2info("AQ0CP00"))
-
-# muestra el análisis morfológico por consola
-def show_mf(sentence):
-  for w in sentence:
-    print("Análisis de: ", w.get_form())
-    for w_a in w.get_analysis():
-      print (w_a.get_lemma())
-      print (w_a.get_tag())
-
-
-# tokenizacion con TweetTokenizer
-tknzr = TweetTokenizer()
-tkn_mensaje = tknzr.tokenize(mensaje)
-print (tkn_mensaje)
-# crear list<word> y sentence
-msg = to_lword(tkn_mensaje)
-msg = freeling.sentence(msg)
-# aplicar análisis morfo
-msg = mf.analyze(msg)
-#show_mf(msg)
+if __name__ == '__main__':
+  mensaje = "El músico bajo toca el bajo"
+  mensaje1 = "Mi #Tbt hoy es con @MarcAnthony  y seguro les gustara quiero que continúen \
+  ustedes con la letra. Cuando nos volvamos a encontrar :)"
+  mensaje2 = "Por fin!! de nuevo en Twitter! =D se me daño mi celular pero ya tengo uno \
+  nuevo =D @rosariomeneses1 creo que es igual al tuyo #emoticones XD <3 ^_^!"
