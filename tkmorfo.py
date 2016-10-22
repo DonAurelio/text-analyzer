726fa42efa832a262be46bf4c6131f71d118b631
BryanTabarez <userbryan@gmail.com>
2016-10-22 02:00:05 +0000

    prueba1

--- tkmorfo.py
+++ tkmorfo.py
@@ -1,8 +1,9 @@
 import freeling
+from nltk.tokenize import TweetTokenizer
 
 
 ## Modify this line to be your FreeLing installation directory
-FREELINGDIR = "/usr/local";
+FREELINGDIR = "/usr";
 
 DATA = FREELINGDIR+"/share/freeling/";
 LANG="es";
@@ -10,9 +11,10 @@ LANG="es";
 freeling.util_init_locale("default");
 
 # create language analyzer
-la=freeling.lang_ident(DATA+"common/lang_ident/ident.dat");
+#la=freeling.lang_ident(DATA+"common/lang_ident/ident.dat");
 
 # create options set for maco analyzer. Default values are Ok, except for data files.
+op= freeling.maco_options("es");
 op.set_data_files( "", 
                    DATA + "common/punct.dat",
                    DATA + LANG + "/dicc.src",
@@ -36,3 +38,9 @@ tg=freeling.hmm_tagger(DATA+LANG+"/tagger.dat",True,2);
 sen=freeling.senses(DATA+LANG+"/senses.dat");
 parser= freeling.chart_parser(DATA+LANG+"/chunker/grammar-chunk.dat");
 dep=freeling.dep_txala(DATA+LANG+"/dep_txala/dependences.dat", parser.get_start_symbol());
+
+# INICIO DE PRUEBA
+tknzr = TweetTokenizer()
+mensaje = """Mi #Tbt hoy es con @MarcAnthony  y seguro les gustara quiero que continúen
+            ustedes con la letra. Cuando nos volvamos a encontrar :)"""
+tknzr.tokenize(mensaje)
