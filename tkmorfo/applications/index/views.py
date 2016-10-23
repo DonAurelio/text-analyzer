6eb271760c81e3852653fe338e9d915b1a6a4b6b
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2016-10-23 06:06:03 +0000

    Pagina web

---
+++ tkmorfo/applications/index/views.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+	template_name = 'index/index.html'
+
