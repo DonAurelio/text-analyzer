6452aa4e1d016191d6f3fd23acb215647c4423fb
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2016-10-23 14:55:54 +0000

    Pagina completa, falta unificación

--- tkmorfo/applications/index/urls.py
+++ tkmorfo/applications/index/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import IndexView
 
 urlpatterns = [
-    url(r'^index/process',IndexView.as_view(),name='index'),
+    url(r'^index/process/',IndexView.as_view(),name='index'),
 ]
--- tkmorfo/applications/index/views.py
+++ tkmorfo/applications/index/views.py
@@ -1,8 +1,29 @@
 # -*- encoding: utf-8 -*-
 
+import json
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+
 
 class IndexView(TemplateView):
 	template_name = 'index/index.html'
 
+	def post(self,request,*args,**kwargs):
+		text = request.POST.get('text',None)
+		preporcesamiento = 'Preprocesamiento'
+		tokenización = 'Tokenización'
+		analisis = 'Analisis Morfologico'
+		template = loader.get_template('index/includes/result.html')
+		context = {}
+		html = template.render(context)
+
+		# Si el campo de texto está vacio entonces se retorna false
+		if len(text) == 0:
+			respuesta = {'status':False,'html':html}
+		else:
+			respuesta = {'status':True,'html':html}
+
+		data = json.dumps(respuesta)
+		return HttpResponse(data,content_type='application/json')
