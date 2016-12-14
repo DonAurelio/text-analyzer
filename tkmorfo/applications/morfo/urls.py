0220784b43ef6d04d15464895cee5294df035c5c
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2016-12-14 03:48:11 +0000

    Modifiación interfaz Analizador Sintactico

--- tkmorfo/applications/morfo/urls.py
+++ tkmorfo/applications/morfo/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import MorfoView
 
 urlpatterns = [
-	url(r'^',MorfoView.as_view(),name='analysis'),
+	url(r'^$',MorfoView.as_view(),name='analysis'),
 ]
--- tkmorfo/applications/textparser/views.py
+++ tkmorfo/applications/textparser/views.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
-
+import json
+from django.template import loader
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 from tools.bikelpreprocessing import split_sentence
 
@@ -9,4 +11,28 @@ class ParserView(TemplateView):
 
 	def post(self,request,*args,**kwargs):
 		text = request.POST.get('text',None)
-		split_sentence(text)
+		model = request.POST.get('radio-modelo',None)
+		mode = request.POST.get('radio-modo',None)
+		test_file_name = request.POST.get('radio-test',None) 
+
+		# Options given by the user throut the template
+		print "Data:", text, model, mode, test_file_name
+
+		# Text Processing
+		# split_sentence(text)
+
+		# Template Rendetization
+		template = loader.get_template('textparser/includes/result_analisys.html')
+		context = {}
+		html = template.render(context)
+
+		# Text validations, if the text field is empty, we return and error or
+		# False status
+		if len(text) == 0:
+			respuesta = {'status':False,'html':html}
+		else:
+			respuesta = {'status':True,'html':html}
+
+		data = json.dumps(respuesta)
+		return HttpResponse(data,content_type='application/json')
+		
