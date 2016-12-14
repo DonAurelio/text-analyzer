d7bc2ee6794b389eb8de051ff2ec7c51a58dc51f
BryanTabarez <userbryan@gmail.com>
2016-12-14 04:36:07 +0000

    Stanford parser...

--- tkmorfo/applications/textparser/tools/bikelpreprocessing.py
+++ tkmorfo/applications/textparser/tools/bikelpreprocessing.py
@@ -61,6 +61,35 @@ def to_bikel_format(tagged_sents):
 			result = result + "("+item[0]+" "+"("+item[1]+")) "
 		result += ") "
 	return result
+
+def parser_stanford(sentences):
+	"""
+	Recibe un conjunto de sentencias, aplica el parser de stanford
+	y retorna el árbol sintáctico // en bracketing -> list(tree)
+	"""
+	return list(list( st_parser.raw_parse_sents(sent_tokenize(sentences)) ))
+
+def parser_bikel(sentences):
+	"""
+	Recibe un conjunto de sentencias, aplica el parser de stanford
+	y retorna el árbol sintáctico en bracketing
+	"""
+	pass
+	
+def parser_output(parser, inputfile, outputfile):
+	"""
+	Lee el archivo de entrada input, genera el árbol sintáctico utilizando
+	el parser, guarda el resultado en el archivo output
+	"""
+	with open(inputfile, 'r') as f:
+		f.readline() # Skip the first line | .START
+		sentences = f.read()
+	trees = parser(sentences)
+	with open(outputfile, 'w') as f:
+		for t in list(trees):
+	 		f.write(str(list(t)[0]).replace("ROOT", "")+"\n")
+
+
 #===============================================================================
 
 
@@ -68,16 +97,47 @@ def to_bikel_format(tagged_sents):
 def main():
 	s = 'Pierre Vinken, 61 years old, will join the board as a nonexecutive \
 		 director Nov. 29.'
+	
 	print "raw_pos_tag:\n", (raw_pos_tag(s))
 
 	#  raw_parse: to parse a sentence. Takes a sentence as a string
 	print "raw_parse:\n", list(st_parser.raw_parse(s))
+	print tagged_parse_sents()
 
 	# raw_parse_sents
-	print "raw_parse_sents:\n", list(list(st_parser.raw_parse_sents(sent_tokenize(s)))[0])
+	# print "raw_parse_sents:\n", list(list(st_parser.raw_parse_sents(sent_tokenize(s)))[0])
 
 	# tagged_parse_sents:  to parse multiple sentences
-	print "tagged_parse_sents:\n", list(list(st_parser.tagged_parse_sents(raw_pos_tag(s)))[0])
+	# print "tagged_parse_sents:\n", list(list(st_parser.tagged_parse_sents(raw_pos_tag(s)))[0])
+
+	"""
+	****************************************************************************
+	PARA STANFORD:
+
+	Leer 00-raw
+	Aplicar parser
+	Generar salida 00-stanford -> wsj_0000.stf
+
+	****************************************************************************
+	
+	****************************************************************************
+	PARA BIKEL:
+	
+	Leer 00-raw y preprocesar
+	
+	Aplicar comando por llamado al sistema:
+	./parse 400 ../settings/collins.properties ../../bikel/wsj-02-21.obj.gz \
+	../../bikel/input-bikel
+
+	Generar salida 00-bikel -> -> wsj_0000.bik
+
+	Comparar archivos seleccionados con 00-gold (parseval)
+	****************************************************************************
+	"""
+
 	
 if __name__ == "__main__":
-	main()
+	#main()
+	inputfile = abs_path + '/wsj_0011'
+	outputfile = abs_path + '/wsj_0011.stf'
+	parser_output(parser_stanford, inputfile, outputfile)
